1123a4acd94eb47c9425d565fe39dbb94b3e3d7d
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-04-15 12:21:39 -0300

    :sparkles: feat: check-in repository validacao de mesmo dia

--- src/repositories/in-memory/in-memory-checkin-repository.ts
+++ src/repositories/in-memory/in-memory-checkin-repository.ts
@@ -1,4 +1,5 @@
 import { Prisma, CheckIn } from "@prisma/client";
+import dayjs from "dayjs";
 import { randomUUID } from "node:crypto";
 import { CheckInsRepository } from "../check-ins-repository";
 
@@ -6,9 +7,16 @@ export class InMemoryCheckInRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
   async findByUserIdOnDate(userId: string, date: Date) {
-    const checkInOnSameDate = this.items.find(
-      (checkIn) => checkIn.user_id === userId
-    );
+    const startOfTheDay = dayjs(date).startOf("date"); // date = dia
+    const endOfTheDay = dayjs(date).endOf("date");
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at);
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+
+      return checkIn.user_id === userId && isOnSameDate;
+    });
 
     if (!checkInOnSameDate) {
       return null;
