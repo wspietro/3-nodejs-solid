ab4f8310f170af0c3e234ec3170608eb35622cf2
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-04-26 11:19:40 -0300

    :construction: feat: jwt recuperando e validando

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,13 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  // Buscar o token dentro do Header
+  // Fazer a validação de signature/secret
+  // Se !token, nada após será executado
+  // Podemos acessar as informações através do request.user
+  await request.jwtVerify();
+
+  console.log(request.user.sub);
+
   return reply.status(200).send();
 }
