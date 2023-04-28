7c4dfab9579b10bdee160e6097cce9198147a106
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-04-28 14:36:17 -0300

    :goal_net: test: register e2e

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,4 +1,24 @@
 /* eslint-disable prettier/prettier */
-import { test } from "vitest";
+import { it, describe, expect, beforeAll, afterAll } from "vitest";
+import request from "supertest";
+import { app } from '@/app'
 
-test("ok", () => { });
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    app.ready()
+  })
+
+  afterAll(async () => {
+    app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'jd@test.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
