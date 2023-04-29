0734f815d5950db88e1e0ba5e341305388a71117
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-04-29 14:27:00 -0300

    :sparkles: feat: search and nearby gym controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeFetchNearbyGymsUseCase } from "@/use-cases/factories/make-fetch-nearby-gyms-use-case";
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    userLatitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    userLongitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { userLatitude, userLongitude } = nearbyGymsQuerySchema.parse(
+    request.body
+  );
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase();
+
+  const { matchingGyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude,
+    userLongitude,
+  });
+
+  return reply.status(201).send({
+    matchingGyms,
+  });
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeSearchGymsUseCase } from "@/use-cases/factories/make-search-gyms-use-case";
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsSchema = z.object({
+    q: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { q, page } = searchGymsSchema.parse(request.body);
+
+  const searchGymsUseCase = makeSearchGymsUseCase();
+
+  const { matchingGyms } = await searchGymsUseCase.execute({
+    query: q,
+    page,
+  });
+
+  return reply.status(201).send({
+    matchingGyms,
+  });
+}
