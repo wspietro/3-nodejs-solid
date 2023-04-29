fe76185c19164938e82768a70c8fb8cc03b2cbdc
Pietro Weg Sera <79719947+wspietro@users.noreply.github.com>
2023-04-29 13:22:33 -0300

    :sparkles: feat: create-gym controller

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
-import { appRoutes } from "./http/routes";
 import fastifyJwt from "@fastify/jwt";
+import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { usersRoutes } from "./http/controllers/users/routes";
 
 export const app = fastify();
 
@@ -11,7 +12,8 @@ app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 });
 
-app.register(appRoutes);
+app.register(usersRoutes);
+app.register(gymsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
